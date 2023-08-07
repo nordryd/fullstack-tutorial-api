434243b4db7ebfbb0437bab61d21687d4a26cebc
BuildTools <unconfigured@null.spigotmc.org>
2023-08-07 13:31:52 -0500

    mongo db added and configured

--- src/main/java/com/nordryd/fullstacktutorialapi/MoviesApplication.java
+++ src/main/java/com/nordryd/fullstacktutorialapi/MoviesApplication.java
@@ -2,8 +2,10 @@ package com.nordryd.fullstacktutorialapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication
 {
     public static void main(final String... args) {
