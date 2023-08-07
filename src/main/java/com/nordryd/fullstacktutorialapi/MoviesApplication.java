97c89da660745bf87f45b857ad7fe83aaba4d10e
BuildTools <unconfigured@null.spigotmc.org>
2023-08-07 11:46:27 -0500

    removed CVE in spring dependencies

--- src/main/java/com/nordryd/fullstacktutorialapi/MoviesApplication.java
+++ src/main/java/com/nordryd/fullstacktutorialapi/MoviesApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieApplication
+public class MoviesApplication
 {
-    public static void main(String[] args) {
-        SpringApplication.run(MovieApplication.class, args);
+    public static void main(final String... args) {
+        SpringApplication.run(MoviesApplication.class, args);
     }
 }
